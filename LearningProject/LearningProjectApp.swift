915cb5b16d4dd20c74de079fa217018ae3764b2f
nathanaelto <nathanael.to@free.fr>
2022-06-27 18:14:43 +0200

    Initial Commit

---
+++ LearningProject/LearningProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningProjectApp.swift
+//  LearningProject
+//
+//  Created by Nathanael TO on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
